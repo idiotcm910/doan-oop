8a40c59ed442b21401632e7f826c77863001b038
idiotcm910 <idiotcm910@gmail.com>
2021-12-04 17:13:45 +0700

    Sua lai cac ham them, sua, xoa, tim kiem

--- src/products/ListProduct.java
+++ src/products/ListProduct.java
@@ -6,117 +6,133 @@ import lib.IListFile;
 import utils.List;
 
 public class ListProduct extends List implements IListFile {
-   private ArrayList<Product> listElement = new ArrayList<Product>();
+   	private ArrayList<Product> listElement = new ArrayList<Product>();
 
-   public void addElement(Product pt) {
-      this.listElement.add(pt);
-   }
+   	public void addElement(Product pt) {
+      	this.listElement.add(pt);
+   	}
 
-   public void addAllElement(ListProduct list) {
-      for(int i = 0; i < list.size(); ++i) {
-         this.listElement.add(list.getElement(i));
-      }
+   	public void addAllElement(ListProduct list) {
+      	for(int i = 0; i < list.size(); ++i) {
+         	this.listElement.add(list.getElement(i));
+      	}
 
-   }
+   	}
 
-   public Product getElement(int index) {
-      return this.listElement.get(index);
-   }
+   	public Product getElement(int index) {
+      	return this.listElement.get(index);
+   	}
 
-   public void setElement(int index, Product pt) {
-      this.listElement.set(index, pt);
-   }
+   	public void setElement(int index, Product pt) {
+      	this.listElement.set(index, pt);
+   	}
 
-   @Override
-   public void xuatDanhSachThongTin() {
-      for(int i = 0; i < this.listElement.size(); ++i) {
-         System.out.printf("%5s%-8s%s%5s\n", " ", i + 1, this.listElement.get(i).xuatThongTin(), " ");
-      }
-   }
+   	@Override
+   	public void xuatDanhSachThongTin() {
+      	for(int i = 0; i < this.listElement.size(); ++i) {
+         	System.out.printf("%5s%-8s%s%5s\n", " ", i + 1, this.listElement.get(i).xuatThongTin(), " ");
+      	}
+   	}
 
-   @Override
-   public int size() {
-      return this.listElement.size();
-   }
+   	@Override
+   	public int size() {
+      	return this.listElement.size();
+   	}
 
-   @Override
-   public boolean containString(String strProperty) {
+   	@Override
+   	public boolean contains(String strProperty) {
         for(Product el : listElement) {
             if(el.contains(strProperty)) {
                 return true;
             }
         }
-        return false;
-   }
 
-   @Override
-   public boolean contains(Object obj) {
+        return false;
+   	}
+
+   	@Override
+   	public boolean contains(Object obj) {
         for(Product el : listElement) {
             if(el.equals(obj)) {
                 return true;
             }
         }
+
         return false;
-   }
+   	}
+	
+   	@Override
+	public int indexOf(String strProperty) {
+		for(int i = 0; i < this.listElement.size(); ++i) {
+			Product el = this.listElement.get(i);
+			
+			if(el.contains(strProperty)) {
+				return i;
+			}
+		}
 
-   @Override
-   public Object findElement(String strProperty) {
-       for(Product el : listElement) {
-            if(el.contains(strProperty)) {
-                return el;
-            }
-       } 
-       return null;
-   }
+		return -1;
+	}
 
-   @Override
-   public int remove(Object obj) {
-      for(int i = 0; i < this.listElement.size(); ++i) {
-         if ((this.listElement.get(i)).equals(obj)) {
-            this.listElement.remove(i);
-            return 1;
-         }
-      }
+	@Override
+	public int indexOf(Object obj) {
+		for(int i = 0; i < this.listElement.size(); ++i) {
+			Product el = this.listElement.get(i);
 
-      return -1;
-   }
+			if(el.equals(obj)) {
+				return i;
+			}
+		}
 
-   @Override
-   public void readListDataInDatabase(ArrayList<String> collection) {
-       for(String data : collection) {
-           Product pt = this.ParseTypeData(data); 
-           this.listElement.add(pt);
-       }
-   }
+		return -1;
+	}
 
-   private Product ParseTypeData(String data) {
-      String typeData = data.substring(0, 2);
-      String contentData = data.substring(2);
+   	@Override
+   	public int remove(int index) {
+		if(index < 0 || index >= this.listElement.size()) { return -1; }
+
+		this.listElement.remove(index);
+		return 1;
+   	}
+
+   	@Override
+   	public void readListDataInDatabase(ArrayList<String> collection) {
+       	for(String data : collection) {
+        	Product pt = this.ParseTypeData(data); 
+           	this.listElement.add(pt);
+       	}
+   	}
+
+   	private Product ParseTypeData(String data) {
+      	String typeData = data.substring(0, 2);
+      	String contentData = data.substring(2);
     
-      switch(typeData) {
-        case "DS":
-            FastFood ff = new FastFood();
-            ff.readDataInDatabase(contentData);
-            return ff;
-        case "FF":
-            Drinks ds = new Drinks();
-            ds.readDataInDatabase(contentData);
+      	switch(typeData) {
+        	case "FF":
+            	FastFood ff = new FastFood();
+            	ff.readDataInDatabase(contentData);
+            	return ff;
+        	case "DS":
+            	Drinks ds = new Drinks();
+            	ds.readDataInDatabase(contentData);
             return ds;
-      }
+      	}
+
         return null;
-   }
+   	}
 
-   @Override
-   public ArrayList<String> writeListDataInDatabase() {
-      ArrayList<String> collection = new ArrayList<String>();
+   	@Override
+   	public ArrayList<String> writeListDataInDatabase() {
+      	ArrayList<String> collection = new ArrayList<String>();
 
-      for(Product el : listElement) {
-           collection.add(addTypeToData(el));
-      }
+      	for(Product el : listElement) {
+           	collection.add(addTypeToData(el));
+      	}
 
-      return collection;
-   }
-    private String addTypeToData(Product pt) {
+      	return collection;
+   	}
+
+ 	private String addTypeToData(Product pt) {
         String typeData = pt.getClass().getSimpleName();
         IFile iF = (IFile)pt;
         String data = "";
--- src/utils/List.java
+++ src/utils/List.java
@@ -2,21 +2,26 @@ package utils;
 
 
 abstract public class List {
-  protected abstract int size();
-  protected abstract void xuatDanhSachThongTin();
+  	protected abstract int size();
+  	protected abstract void xuatDanhSachThongTin();
 
-   // hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của một đối tượng
-   // thuộc danh sách không.
-  protected abstract boolean containString(String strProperty);
+	// ====== Các hàm tìm kiếm ======
+   	// hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của một đối tượng
+   	// thuộc danh sách không.
+  	protected abstract boolean contains(String strProperty);
+	
+	// hàm này sẽ kiểm tra xem đối tượng có thuộc danh sách không.
+	protected abstract boolean contains(Object obj);
+	
+	// hàm tìm kiếm phần tử dựa trên đối tượng truyền vào 
+	// tìm thấy sẽ trả về chỉ mục của phần tử đó
+	// không tìm thấy trả về -1
+	protected abstract int indexOf(Object obj);
 
-   // hàm này sẽ kiểm tra xem đối tượng có thuộc danh sách không.
-  protected abstract boolean contains(Object obj);
+	// hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của phần tử trong danh sách không 
+	// nếu có thì trả về chỉ mục của phần tử đó, không trả về -1
+	protected abstract int indexOf(String strProperty);
 
-  // hàm này sẽ tìm kiếm xem chuỗi có phải là 1 thuộc tính của một đối tượng không,
-  // nếu có trả về đối tượng đó
-  // không trả về null
-  protected abstract Object findElement(String strProperty);
-  
-  // hàm xóa phần tử, xóa thành công trả về 1, không thành công trả về -1
-  protected abstract int remove(Object objRm);
+  	// hàm xóa phần tử.
+  	protected abstract int remove(int index);
 }
