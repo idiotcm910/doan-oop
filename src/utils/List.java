dd109e5ddd1973e1c5736d77d58a2c4976463e4c
quoc thai <idiotcm910@gmail.com>
2021-12-07 21:02:10 +0700

    Thêm doc comment cho class

--- src/utils/List.java
+++ src/utils/List.java
@@ -6,22 +6,34 @@ abstract public class List {
   	protected abstract void xuatDanhSachThongTin();
 
 	// ====== Các hàm tìm kiếm ======
-   	// hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của một đối tượng
-   	// thuộc danh sách không.
+   	/** 
+	 * hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của một đối tượng thuộc danh sách không.
+	 * @param obj đối số nhận vào 1 đối tượng
+	 * */
   	protected abstract boolean contains(String strProperty);
-	
-	// hàm này sẽ kiểm tra xem đối tượng có thuộc danh sách không.
+
+	/** 
+	 * hàm này sẽ kiểm tra xem đối tượng có thuộc danh sách không.
+	 * @param obj đối số nhận vào 1 đối tượng
+	 * */
 	protected abstract boolean contains(Object obj);
 	
-	// hàm tìm kiếm phần tử dựa trên đối tượng truyền vào 
-	// tìm thấy sẽ trả về chỉ mục của phần tử đó
-	// không tìm thấy trả về -1
+	/** 
+	 * hàm tìm kiếm phần tử dựa trên đối tượng truyền vào 
+	 * @param obj đối số nhận vào 1 đối tượng
+	 * @return int trả về chỉ mục của phần tử nếu tìm thấy, không tìm thấy trả về -1
+	 * */
 	protected abstract int indexOf(Object obj);
 
-	// hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của phần tử trong danh sách không 
-	// nếu có thì trả về chỉ mục của phần tử đó, không trả về -1
+	/**
+	 * hàm này sẽ kiểm tra xem một chuỗi có phải là 1 thuộc tính của phần tử trong danh sách không 
+	 * @param strProperty đối số nhận vào 1 chuỗi ký tự
+	 * @return int trả về chỉ mục của phần tử được tìm thấy, không tìm thấy trả về -1
+	 * */
 	protected abstract int indexOf(String strProperty);
 
-  	// hàm xóa phần tử.
+	/**
+  	 * hàm xóa phần tử.
+	 * */
   	protected abstract int remove(int index);
 }
